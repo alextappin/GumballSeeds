0ac00076b0cdc5c8805df3768d71ee947180615f
t_tappa <alex.tappin@autodesk.com>
2015-10-01 17:08:22 -0700

    Added a mapClass to the project.

---
+++ ScrollerWorld/Src/js/classes/MapClass.js
@@ -0,0 +1,23 @@
+/**
+ * Created by t_tappa on 10/1/2015.
+ */
+
+function MapClass() {
+    var stage = ContainerHelper().newContainer(0x999888),
+        farTexture = TextureHelper().newTexture('bg-far.png'),
+        midTexture = TextureHelper().newTexture('bg-mid.png'),
+        far = TileSpriteHelper().newTiling(farTexture, 512, 256),
+        mid = TileSpriteHelper().newTiling(midTexture, 512, 256);
+    TileSpriteHelper().setPosition(far, 0, 0);
+    TileSpriteHelper().setTilePosition(far, 0, 0);
+    TileSpriteHelper().setPosition(mid, 0, 128);
+    TileSpriteHelper().setTilePosition(mid, 0, 0);
+    stage = ContainerHelper().addChildren(stage, far, mid);
+    return {
+        stage : stage,
+        farTexture : farTexture,
+        midTexture : midTexture,
+        far : far,
+        mid : mid
+    }
+}
--- ScrollerWorld/Src/js/game.js
+++ ScrollerWorld/Src/js/game.js
@@ -1,31 +1,17 @@
 /**
  * Created by t_tappa on 9/30/2015.
  */
-var renderer = PIXI.autoDetectRenderer(512, 384,{backgroundColor : 0x66FF99});
+var renderer = PIXI.autoDetectRenderer(512, 384,{backgroundColor : 0x66FF99}),
+    map = MapClass();
 document.body.appendChild(renderer.view);
-
-var stage = ContainerHelper().newContainer(0x999888);
-var farTexture = TextureHelper().newTexture('bg-far.png');
-var midTexture = TextureHelper().newTexture('bg-mid.png');
-
-var far = TileSpriteHelper().newTiling(farTexture, 512, 256);
-var mid = TileSpriteHelper().newTiling(midTexture, 512, 256);
-
-TileSpriteHelper().setPosition(far, 0, 0);
-TileSpriteHelper().setTilePosition(far, 0, 0);
-
-TileSpriteHelper().setPosition(mid, 0, 128);
-TileSpriteHelper().setTilePosition(mid, 0, 0);
-
-stage = ContainerHelper().addChildren(stage, far, mid);
 
 requestAnimationFrame(update);
 
 function update() {
-    far.tilePosition.x -= 0.128;
-    mid.tilePosition.x -= 0.64;
+    map.far.tilePosition.x -= 0.128;
+    map.mid.tilePosition.x -= 0.64;
 
-    renderer.render(stage);
+    renderer.render(map.stage);
 
     requestAnimationFrame(update);
 }
