0c452572303b7ec41a5c8a95e2388e9195bc9016
alextappin <alextappin@gmail.com>
2016-03-10 00:13:58 -0800

    Fixed the constants. It is now a variable.

--- ScrollerWorld/Src/js/GameVariables.js
+++ ScrollerWorld/Src/js/GameVariables.js
@@ -1,25 +1,47 @@
 /**
  * Created by ajt on 3/9/2016.
  */
-function GameVariables() {
+var GameVariables = (function GameVariables() {
     var constants = {
         screenWidth : 1080,
         screenHeight : 720,
-        enemies : 2
+        enemies : 2,
+        minScrollSpeed : 5.5,
+        maxScrollSpeed : 7,
+        scrollAcceleration : 0.005,
+        currentScrollSpeed : 0
     };
 
     return {
+        //getters
         getWidth : function getWidth() {
             return constants.screenWidth;
         },
         getHeight : function getHeight() {
             return constants.screenHeight;
         },
+        getEnemies : function getEnemies() {
+            return constants.enemies;
+        },
+        getMinScrollSpeed : function getMinScrollSpeed() {
+            return constants.minScrollSpeed;
+        },
+        getMaxScrollSpeed : function getMaxScrollSpeed() {
+            return constants.maxScrollSpeed;
+        },
+        getScrollAcceleration : function getScrollAcceleration() {
+            return constants.scrollAcceleration;
+        },
+        getCurrentScrollSpeed : function getCurrentScrollSpeed() {
+            return constants.currentScrollSpeed;
+        },
+
+        //setters
         setEnemies : function setEnemies(value) {
             constants.enemies = value;
         },
-        getEnemies : function getEnemies() {
-            return constants.enemies;
+        setCurrentScrollSpeed : function setCurrentScrollSpeed(value) {
+            constants.currentScrollSpeed = value;
         }
     };
-}
+})();
