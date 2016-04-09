e8fbd8d941cb599648619ce5d87ab5e1eddd207d
alextappin <alextappin@gmail.com>
2016-04-09 00:10:01 -0700

    Title board properties renamed.

--- js/UnitsSprites/TitleBoard.js
+++ js/UnitsSprites/TitleBoard.js
@@ -10,7 +10,7 @@ TitleBoard.constructor = TitleBoard;
 TitleBoard.prototype = Object.create(PIXI.Container.prototype);
 
 TitleBoard.prototype.constructTitleBoard = function() {
-    this.TitleBoardProperties = new TitleBoardProperties();
+    this.Properties = new TitleBoardProperties();
     this.initiateTitleBoardSprites();
 };
 TitleBoard.prototype.setPositionAndScale = function(obj) {
@@ -18,36 +18,36 @@ TitleBoard.prototype.setPositionAndScale = function(obj) {
     //no scale yet...
 };
 TitleBoard.prototype.initiateTitleBoardSprites = function() {
-    this.TitleBoardProperties.textures.push(
+    this.Properties.textures.push(
         PIXI.Texture.fromFrame("Title1"),
         PIXI.Texture.fromFrame("Title2")
     );
-    this.TitleBoardProperties.sprite = new PIXI.Sprite(this.TitleBoardProperties.textures[this.TitleBoardProperties.spriteCount]);
+    this.Properties.sprite = new PIXI.Sprite(this.Properties.textures[this.Properties.spriteCount]);
     //add them to the array
-    //this.TitleBoardProperties.boardSprites.push(sprite1,sprite2);
-    this.addChild(this.TitleBoardProperties.sprite);
+    //this.Properties.boardSprites.push(sprite1,sprite2);
+    this.addChild(this.Properties.sprite);
 };
 TitleBoard.prototype.setSpriteToCurrentTexture = function() {
-    this.TitleBoardProperties.sprite.texture = this.TitleBoardProperties.textures[this.TitleBoardProperties.spriteCount];
+    this.Properties.sprite.texture = this.Properties.textures[this.Properties.spriteCount];
 };
 TitleBoard.prototype.update = function(titleBoardObj) {
     this.updateSprites();
 };
 TitleBoard.prototype.updateSprites = function() {
-    if (this.TitleBoardProperties.changeSpriteCounter == this.TitleBoardProperties.spriteSpeed) {
-        this.TitleBoardProperties.changeSpriteCounter = 0;
+    if (this.Properties.changeSpriteCounter == this.Properties.spriteSpeed) {
+        this.Properties.changeSpriteCounter = 0;
         this.nextSprite();
     }
     else {
-        this.TitleBoardProperties.changeSpriteCounter++;
+        this.Properties.changeSpriteCounter++;
     }
 };
 TitleBoard.prototype.nextSprite = function() {
-    if (this.TitleBoardProperties.spriteCount == 1) {
-        this.TitleBoardProperties.spriteCount = 0;
+    if (this.Properties.spriteCount == 1) {
+        this.Properties.spriteCount = 0;
     }
     else {
-        this.TitleBoardProperties.spriteCount++;
+        this.Properties.spriteCount++;
     }
     this.setSpriteToCurrentTexture();
 };
