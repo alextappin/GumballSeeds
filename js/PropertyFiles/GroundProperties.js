23973a8e1b36db34d03286dc786d24730c5b695f
alextappin <alextappin@gmail.com>
2016-04-10 18:09:06 -0700

    Added the properties file innards.

--- js/PropertyFiles/GroundProperties.js
+++ js/PropertyFiles/GroundProperties.js
@@ -1,3 +1,16 @@
 /**
  * Created by ajt on 4/10/2016.
  */
+function GroundProperties() {
+    var props = {
+        sprite : {},
+        textures : [],
+        spriteCount : 0,
+        changeSpriteCounter : 0,
+        spriteSpeed : 40
+    };
+
+    return props;
+}
+
+GroundProperties.constructor = GroundProperties;
