f2b848c2ab938eff0357f8e7a343af7915e9d4b5
alextappin <alextappin@gmail.com>
2016-04-11 12:43:38 -0700

    Added some new logic to the adding and moving of the sprites in the Ground.js

--- js/MapControl/Ground.js
+++ js/MapControl/Ground.js
@@ -14,32 +14,34 @@ Ground.prototype.constructGround = function() {
     this.initiateGroundSprites();
 };
 Ground.prototype.setPositionAndScale = function(obj) {
-    obj.position =  GameVariables.getNewPoint((GameVariables.getWidth() - obj.width)/2, (GameVariables.getHeight() - obj.height)/2);
+    obj.position.y =  300;
     //no scale yet...
 };
 Ground.prototype.initiateGroundSprites = function() {
     this.Properties.textures.push(
-        PIXI.Texture.fromFrame("Title1"),
-        PIXI.Texture.fromFrame("Title2")
+        PIXI.Texture.fromFrame("fg5New")
     );
-    this.Properties.sprite = new PIXI.Sprite(this.Properties.textures[this.Properties.spriteCount]);
-    this.addChild(this.Properties.sprite);
+    //will have around 4 sprites for the ground. The ground will be moving. In the future, it may be better to have 2...
+    for (var i = 0; i < this.Properties.numberOfSprites; i++) {
+        this.Properties.sprites.push(new PIXI.Sprite(this.Properties.textures[0]));
+        this.addChild(this.Properties.sprites[i]);
+    }
+
 };
 Ground.prototype.setSpriteToCurrentTexture = function() {
     this.Properties.sprite.texture = this.Properties.textures[this.Properties.spriteCount];
 };
-Ground.prototype.update = function(titleBoardObj) {
-    this.updateSprites();
+Ground.prototype.update = function(obj) {
+    this.updateSprites(obj);
 };
-Ground.prototype.updateSprites = function() {
-    if (this.Properties.changeSpriteCounter == this.Properties.spriteSpeed) {
-        this.Properties.changeSpriteCounter = 0;
-        this.nextSprite();
+Ground.prototype.updateSprites = function(obj) {
+    for (var i = 0; i < this.Properties.numberOfSprites; i++) {
+        this.Properties.sprites[i].position.x -= 10;
+        if (this.Properties.sprites[i].position.x + this.Properties.spriteWidth < 0) {
+            this.Properties.sprites[i].position.x = this.Properties.spriteWidth * this.Properties.spriteCount;
+        }
     }
-    else {
-        this.Properties.changeSpriteCounter++;
-    }
-};
+};/*
 Ground.prototype.nextSprite = function() {
     if (this.Properties.spriteCount == 1) {
         this.Properties.spriteCount = 0;
@@ -48,4 +50,4 @@ Ground.prototype.nextSprite = function() {
         this.Properties.spriteCount++;
     }
     this.setSpriteToCurrentTexture();
-};
+};*/
