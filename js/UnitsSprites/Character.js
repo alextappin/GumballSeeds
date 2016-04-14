a5f8bc78237c0dd0ce61cb0e2d705fabc9551935
alextappin <alextappin@gmail.com>
2016-04-14 01:30:27 -0700

    Character new gravity working. Should be better.

--- js/UnitsSprites/Character.js
+++ js/UnitsSprites/Character.js
@@ -59,28 +59,35 @@ Character.prototype.nextSprite = function() {
     this.setSpriteToCurrentTexture();
 };
 Character.prototype.characterGravity = function(characterObj, groundObj) {
-    //if there is no ground,
-    if (!groundObj.getHeightAtPositionX(this.Properties.sprite.position.x)) {
-        this.Properties.airborn = true;
-        this.gravitateAndCheckIfLanded(characterObj, groundObj.getHeightAtPositionX(this.Properties.sprite.position.x));
+    if (this.Properties.airborn) {
+        this.gravitateAndCheckIfLanded(characterObj, groundObj.getHeightAtPositionX(characterObj.position.x));
     }
-    else if (this.Properties.airborn) {
-        this.gravitateAndCheckIfLanded(characterObj, groundObj.getHeightAtPositionX(this.Properties.sprite.position.x));
+    //if there is no ground,
+    else if (!groundObj.getHeightAtPositionX(this.Properties.sprite.position.x)) {
+        this.Properties.airborn = true;
+        this.gravitateAndCheckIfLanded(characterObj, groundObj.getHeightAtPositionX(characterObj.position.x));
     }
 };
 Character.prototype.gravitateAndCheckIfLanded = function(characterObj, groundHeight) {
+    //get new velocity
+    this.startGravity();
     //if falling, check if lower than ground, then check if next position is lower than ground, set character on ground
     if (this.isFalling()) {
-        if(this.Properties.sprite.position.x > groundHeight) {
+        if(characterObj.position.y > groundHeight) {
             this.endGame();
+            characterObj.position.y += this.Properties.velocityY;
         }
-        else if (this.startGravityGetPosition() > groundHeight) {
+        else if ((characterObj.position.y + this.Properties.velocityY) > groundHeight) {
             characterObj.position.y = groundHeight;
+            this.Properties.airborn = false;
+        }
+        else {
+            characterObj.position.y += this.Properties.velocityY;
         }
     }
     //if not falling, you are rising at this point, apply gravity.
     else {
-        characterObj.position.y = this.startGravityGetPosition();
+        characterObj.position.y += this.Properties.velocityY;
     }
 };
 Character.prototype.isFalling = function() {
@@ -88,21 +95,12 @@ Character.prototype.isFalling = function() {
     return this.Properties.velocityY > 0;
 };
 Character.prototype.endGame = function() {
-
+    this.Properties.continueGame = false;
+    GameVariables.toggleScreenChange();
+    GameVariables.setScreenTitle();
 };
-Character.prototype.startGravityGetPosition = function() {
+Character.prototype.startGravity = function() {
     this.Properties.velocityY += this.Properties.gravity;
-    return this.Properties.sprite.position.y + this.Properties.velocityY;
-};
-
-Character.prototype.jumpCharacter = function(characterObj, groundObj){
-    if (this.charIsJumping()) {
-        characterObj.position.y = this.moveHeightJumping(characterObj.position.y,
-            groundObj.Properties.positionY, groundObj.Properties.positionY);
-    }
-    else {
-        this.endJumping();
-    }
 };
 Character.prototype.attackCharacter = function() {
     if (this.Properties.isAttacking) {
@@ -119,43 +117,8 @@ Character.prototype.startJumpAnimation = function() {
         this.Properties.velocityY = this.Properties.jumpVelocity;
     }
 };
-Character.prototype.moveHeightJumping = function(posY, currentSlicePosY, nextSlicePosY) {
-    return this.simulateGravity(posY, this.calculateMapToCharacterHeightOffset(currentSlicePosY), this.calculateMapToCharacterHeightOffset(nextSlicePosY));
-};
-Character.prototype.simulateGravity = function(posY, currentSlicePosY, nextSlicePosY) {
-    this.Properties.velocityY += this.Properties.gravity;
-    posY += this.Properties.velocityY;
-
-    //TODO put these numbers in a config file. This number signifies the lowest wall
-    //if the character isnt moving up and the next slice is taller than the character...
-    //if the character is lower than the next slice, the character is travelling down and they are over a gap (99976) then the game is over
-    if (posY > nextSlicePosY+100 && this.Properties.velocityY > 0) {
-        this.Properties.continueGame = false;
-        GameVariables.toggleScreenChange();
-        GameVariables.setScreenTitle();
-    }
-    //TODO psyY > currentSlicePos & velocity is positive(negative...)
-    if (posY >= currentSlicePosY && this.Properties.continueGame) {
-        this.Properties.velocityY = 0.0;
-        this.Properties.jumping = false;
-        posY = currentSlicePosY;
-    }
-    return posY;
-};
-Character.prototype.endJumping = function() {
-    this.Properties.jumping = false;
-};
-Character.prototype.charIsJumping = function() {
-    return (this.Properties.jumping);
-};
 Character.prototype.calculateMapToCharacterHeightOffset = function(wallPos) {
     return wallPos - 52;
-};
-Character.prototype.checkIfFalling = function(currentSliceHeight, nextSliceHeight) {
-    if (!this.Properties.jumping && this.calculateMapToCharacterHeightOffset(currentSliceHeight) > this.position.y) {
-        this.Properties.jumping = true;
-        this.simulateGravity(this.position.y, this.calculateMapToCharacterHeightOffset(currentSliceHeight), this.calculateMapToCharacterHeightOffset(nextSliceHeight));
-    }
 };
 Character.prototype.startAttackAnimation = function() {
     this.Properties.isAttacking = true;
@@ -175,9 +138,7 @@ Character.prototype.stopAttacking = function() {
 Character.prototype.listenForJumpTrigger = function() {
     var that = this;
     this.Properties.spaceBar.press = function () {
-        if (!that.Properties.jumping) {
-            that.startJumpAnimation();
-        }
+        that.startJumpAnimation();
     }
 };
 Character.prototype.listenForAttackTrigger = function() {
