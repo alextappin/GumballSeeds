f198b56d8727f760bd971a41ece0f25139057b2d
Alex Tappin <alextappin@gmail.com>
2016-04-14 17:18:08 -0700

    Initialized the properties of the Gumball

--- js/PropertyFiles/GumballProperties.js
+++ js/PropertyFiles/GumballProperties.js
@@ -1,3 +1,16 @@
 /**
  * Created by ajt on 3/12/2016.
  */
+function GumballProperties() {
+    var props = {
+        sprite : {},
+        textures : [],
+        spriteCount : 0,
+        changeSpriteCounter : 0,
+        spriteSpeed : 40
+    };
+
+    return props;
+}
+
+GumballProperties.constructor = GumballProperties;
