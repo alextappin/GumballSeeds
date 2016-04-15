2a8f5e8a392acfa133283c1409244a1cb0d0d088
Alex Tappin <alextappin@gmail.com>
2016-04-14 17:18:25 -0700

    Gumball.js class is setup and ready to be implemented.

--- js/UnitsSprites/Gumball.js
+++ js/UnitsSprites/Gumball.js
@@ -1,37 +1,36 @@
 /**
  * Created by ajt on 3/12/2016.
  */
-function TitleBoard() {
+function Gumball() {
     PIXI.Container.call(this);
-    this.constructTitleBoard();
+    this.constructGumball();
 }
 
-TitleBoard.constructor = TitleBoard;
-TitleBoard.prototype = Object.create(PIXI.Container.prototype);
+Gumball.constructor = Gumball;
+Gumball.prototype = Object.create(PIXI.Container.prototype);
 
-TitleBoard.prototype.constructTitleBoard = function() {
-    this.Properties = new TitleBoardProperties();
-    this.initiateTitleBoardSprites();
+Gumball.prototype.constructGumball = function() {
+    this.Properties = new GumballProperties();
+    this.initiateGumballSprites();
 };
-TitleBoard.prototype.setPositionAndScale = function(obj) {
-    obj.position =  GameVariables.getNewPoint((GameVariables.getWidth() - obj.width)/2, (GameVariables.getHeight() - obj.height)/2);
+Gumball.prototype.setPositionAndScale = function(obj) {
+    obj.position =  GameVariables.getNewPoint(200,300);
     //no scale yet...
 };
-TitleBoard.prototype.initiateTitleBoardSprites = function() {
+Gumball.prototype.initiateGumballSprites = function() {
     this.Properties.textures.push(
-        PIXI.Texture.fromFrame("Title1"),
-        PIXI.Texture.fromFrame("Title2")
+        PIXI.Texture.fromFrame("RedGumball")
     );
     this.Properties.sprite = new PIXI.Sprite(this.Properties.textures[this.Properties.spriteCount]);
     this.addChild(this.Properties.sprite);
 };
-TitleBoard.prototype.setSpriteToCurrentTexture = function() {
+Gumball.prototype.setSpriteToCurrentTexture = function() {
     this.Properties.sprite.texture = this.Properties.textures[this.Properties.spriteCount];
 };
-TitleBoard.prototype.update = function(titleBoardObj) {
+Gumball.prototype.update = function(titleBoardObj) {
     this.updateSprites();
 };
-TitleBoard.prototype.updateSprites = function() {
+Gumball.prototype.updateSprites = function() {
     if (this.Properties.changeSpriteCounter == this.Properties.spriteSpeed) {
         this.Properties.changeSpriteCounter = 0;
         this.nextSprite();
@@ -40,7 +39,7 @@ TitleBoard.prototype.updateSprites = function() {
         this.Properties.changeSpriteCounter++;
     }
 };
-TitleBoard.prototype.nextSprite = function() {
+Gumball.prototype.nextSprite = function() {
     if (this.Properties.spriteCount == 1) {
         this.Properties.spriteCount = 0;
     }
