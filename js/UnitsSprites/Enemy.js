3562be1ec94033487592cff5999924833b717dee
alextappin <alextappin@gmail.com>
2016-04-14 23:11:19 -0700

    format enemy.js

--- js/UnitsSprites/Enemy.js
+++ js/UnitsSprites/Enemy.js
@@ -121,5 +121,4 @@ Enemy.prototype.isIntersecting = function(rectangle1, rectangle2) {
     (rectangle2.position.x + rectangle2.width) < rectangle1.x ||
     rectangle2.position.y > (rectangle1.position.y + rectangle1.height) ||
     (rectangle2.position.y + rectangle2.height) < rectangle1.position.y);
-
 };
