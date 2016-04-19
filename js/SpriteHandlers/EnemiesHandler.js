d1926c27e5f430e9687c2aa7afe2fdca15eba18c
alextappin <alextappin@gmail.com>
2016-04-18 22:13:49 -0700

    Added basic functions into the enemiesHandler.js

--- js/SpriteHandlers/EnemiesHandler.js
+++ js/SpriteHandlers/EnemiesHandler.js
@@ -1,3 +1,28 @@
 /**
  * Created by ajt on 4/17/2016.
  */
+function EnemiesHandler() {
+    this.constructEnemies();
+}
+
+EnemiesHandler.constructor = EnemiesHandler;
+
+EnemiesHandler.prototype.update = function() {
+
+};
+
+EnemiesHandler.prototype.constructEnemies = function() {
+
+};
+
+EnemiesHandler.prototype.setPositionAndScale = function() {
+
+};
+
+EnemiesHandler.prototype.addEnemy = function() {
+
+};
+
+EnemiesHandler.prototype.removeEnemy = function() {
+
+};
