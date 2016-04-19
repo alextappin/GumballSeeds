2fe23209b45994720ca58873370fe9cfbbfad4cf
alextappin <alextappin@gmail.com>
2016-04-18 23:00:30 -0700

    Enemy handler should be completed.

--- js/SpriteHandlers/EnemiesHandler.js
+++ js/SpriteHandlers/EnemiesHandler.js
@@ -2,29 +2,12 @@
  * Created by ajt on 4/17/2016.
  */
 function EnemiesHandler() {
+    //array of enemy objects
     this.enemies = [];
     this.constructEnemies();
 }
 
 EnemiesHandler.constructor = EnemiesHandler;
-
-EnemiesHandler.prototype.update = function(enemyHandler, characterObj) {
-/*    for (var n = 0; n < GameVariables.getEnemies(); n++) {
-        if (this.Properties.enemies[n]) {
-            this.Properties.enemies[n].update(this.Properties.enemies[n], this.Properties.character);
-        }
-        //if there are not enough enemies, add another
-        else {
-            this.createEnemies(1, this.getStage());
-        }
-    }*/
-
-
-
-    for (var n = 0; n < this.enemies.length; n++) {
-        this.enemies[n].update(this.enemies[n], characterObj);
-    }
-};
 
 EnemiesHandler.prototype.constructEnemies = function() {
     for (var n = 0; n < GameVariables.getEnemies(); n++) {
@@ -34,27 +17,39 @@ EnemiesHandler.prototype.constructEnemies = function() {
 
 EnemiesHandler.prototype.setPositionAndScale = function() {
     for (var n = 0; n < this.enemies.length; n++) {
-        this.enemies[n].setPositionAndScale();
+        this.enemies[n].setPositionAndScale(this.enemies[n]);
     }
 };
 
-EnemiesHandler.prototype.addEnemy = function(stage) {
-    this.enemies.push(new Enemy);
-    stage.addChild(this.enemies[this.enemies.length - 1]);
-    GameVariables.setEnemies(GameVariables.getEnemies() + 1);
+EnemiesHandler.prototype.addEnemiesToStage = function(stage) {
+    for (var n = 0; n < this.enemies.length; n++) {
+        stage.addChild(this.enemies[n]);
+    }
+};
+
+EnemiesHandler.prototype.update = function(enemyHandler, characterObj, stage) {
+    for (var n = 0; n < GameVariables.getEnemies(); n++) {
+        if (enemyHandler.enemies[n]) {
+            enemyHandler.enemies[n].update(enemyHandler.enemies[n], characterObj);
+        }
+        //if there are not enough enemies, add another
+        else {
+            this.addEnemy(1, stage);
+        }
+        enemyHandler.enemies[n].update(enemyHandler.enemies[n], characterObj);
+    }
+};
+
+EnemiesHandler.prototype.addEnemy = function(numberToAdd, stage) {
+    for (var n = 0; n < numberToAdd; n++) {
+        this.enemies.push(new Enemy);
+        stage.addChild(this.enemies[this.enemies.length - 1]);
+    }
 };
 
 EnemiesHandler.prototype.removeEnemy = function(index, stage) {
     if (this.enemies.length > 0) {
         stage.removeChild(this.enemies[index]);
         this.enemies.splice(index, 1);
-    }
-
-    GameVariables.setEnemies(GameVariables.getEnemies() + 1);
-};
-
-EnemiesHandler.prototype.addEnemiesToStage = function(stage) {
-    for (var n = 0; n < this.enemies.length; n++) {
-        stage.addChild(this.enemies[n]);
     }
 };
