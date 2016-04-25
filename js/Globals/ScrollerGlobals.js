1681db929be26c1399c226b1479052a2f824f155
alextappin <alextappin@gmail.com>
2016-04-25 14:28:33 -0700

    implemented the scroller Globals class

--- js/Globals/ScrollerGlobals.js
+++ js/Globals/ScrollerGlobals.js
@@ -2,5 +2,13 @@
  * Created by ajt on 4/25/2016.
  */
 var ScrollerGlobals = (function ScrollerGlobals() {
+    var constants = {};
 
+    constants.minScrollSpeed = 5.5;
+    constants.maxScrollSpeed = 7;
+    constants.currentScrollSpeed = 20;
+    constants.scrollAcceleration = 0.005;
+    constants.groundSpeed = 10;
+
+    return constants;
 })();
