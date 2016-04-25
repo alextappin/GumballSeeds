e477a7de77e5b531220c097253464871ceec5c7a
alextappin <alextappin@gmail.com>
2016-04-25 14:27:54 -0700

    added and implemented the ScoreGlobals class

---
+++ js/Globals/ScoreGlobals.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ajt on 4/25/2016.
+ */
+var ScoreGlobals = (function ScoreGlobals() {
+    var constants = {};
+
+    constants.currentScore = 0;
+    constants.highScore = 0;
+    constants.lives = 30;
+    constants.kills = 0;
+
+    return constants;
+})();
