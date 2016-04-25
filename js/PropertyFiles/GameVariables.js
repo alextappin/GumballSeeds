967ebc12be0756efd9467d46db8dab79a9538674
alextappin <alextappin@gmail.com>
2016-04-25 02:15:54 -0700

    updated the duration for testing

--- js/PropertyFiles/GameVariables.js
+++ js/PropertyFiles/GameVariables.js
@@ -27,7 +27,7 @@ var GameVariables = (function GameVariables() {
         maxPowerBar : 6,
         powerUpActive : false,
         powerUpStartingViewPort : 0,
-        powerUpDuration : 10000
+        powerUpDuration : 1000
     };
 
     return {
