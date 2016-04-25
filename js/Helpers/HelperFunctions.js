1d5f4cfe721e14146f75206ca50f50c75166839c
alextappin <alextappin@gmail.com>
2016-04-25 14:53:07 -0700

    Added a helper function for resetting variables.

--- js/Helpers/HelperFunctions.js
+++ js/Helpers/HelperFunctions.js
@@ -1,16 +1,34 @@
 /**
  * Created by ajt on 4/25/2016.
  */
-function HelperFunction() {
+var HelperFunctions = (function HelperFunction() {
     var functions = {};
 
     functions.getRandomNumber = function(startRange, endRange) {
         //Will return a random number from startRange to endRange
         return Math.floor((Math.random() * endRange) + startRange);
     };
+
     functions.getNewPoint = function(x, y) {
         return {x : x, y : y};
     };
 
+    functions.resetGlobals = function() {
+        if (ScoreGlobals.currentScore > ScoreGlobals.highScore) {
+            ScoreGlobals.highScore = ScoreGlobals.currentScore;
+        }
+
+        BalanceGlobals.enemies = 2;
+        BalanceGlobals.gumballs = 1;
+        ScoreGlobals.lives = 20;
+        ScoreGlobals.kills = 0;
+        ScoreGlobals.score = 0;
+        MapGlobals.loopCounter = 0;
+        ScrollerGlobals.groundSpeed = 10;
+        PowerUpGlobals.powerBarLevel = 2;
+        PowerUpGlobals.powerUpActive = false;
+        PowerUpGlobals.powerUpStartingViewport = 0;
+    };
+
     return functions;
-}
+})();
