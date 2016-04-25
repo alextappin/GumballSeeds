13db7b890058763f73b3aa5ed62f18cec95c5626
alextappin <alextappin@gmail.com>
2016-04-25 14:28:06 -0700

    Implemented the balanceGlobals class

--- js/Globals/BalanceGlobals.js
+++ js/Globals/BalanceGlobals.js
@@ -2,5 +2,10 @@
  * Created by ajt on 4/25/2016.
  */
 var BalanceGlobals = (function BalanceGlobals() {
+    var constants = {};
 
+    constants.enemies = 2;
+    constants.gumball = 1;
+    constants.createNewEnemiesCounter = 5;
+    constants.loopScoreIncrementTime = 60;
 })();
