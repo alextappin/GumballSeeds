9ae39fbe7b7c7c48cdced0cef17c9b9a747db865
alextappin <alextappin@gmail.com>
2016-04-27 17:57:50 -0700

    adding text to the load screen.

--- js/PropertyFiles/TextProperties.js
+++ js/PropertyFiles/TextProperties.js
@@ -24,6 +24,23 @@ function TextProperties(textType) {
                 this.positionY = 40;
                 this.fill = "Yellow";
             }
+            else if(this.type == "loadInstructions") {
+                //this.text = "Tap left side of the screen to jump \n Right side of the screen to Attack"
+                this.text = "Tap left side of the screen to jump \n Right side of the screen to Attack";
+                this.positionX = 10;
+                this.positionY = 40;
+                this.fill = "White";
+                this.wordWrap = true;
+                this.wordWrapWidth = MapGlobals.screenWidth/3;
+            }
+            else if(this.type = "loadInfo") {
+                this.text = "Only five guardians called GUMBALL SEEDS can save the sweet gumball world from a dark and sour transformation led by KING SOUR and his minions of TWISTED SOURS, but first the GUMBALL SEED siblings must overcome their own sourness and reunite as a family to turn everything sour back to sweet!";
+                this.positionX = MapGlobals.screenWidth - MapGlobals.screenWidth/4;
+                this.positionY = 40;
+                this.fill = "Red";
+                this.wordWrap = true;
+                this.wordWrapWidth = MapGlobals.screenWidth/4;
+            }
             else {
                 this.text = "DEFAULT";
                 this.positionX = 0;
@@ -33,7 +50,9 @@ function TextProperties(textType) {
         },
         getStyleProperties : function() {
             return {
-                fill : this.fill
+                fill : this.fill,
+                wordWrap : this.wordWrap,
+                wordWrapWidth : this.wordWrapWidth
             }
 
         }
