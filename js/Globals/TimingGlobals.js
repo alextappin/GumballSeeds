c59c71a94c54b661c67a835518028b0fc816822d
alextappin <alextappin@gmail.com>
2016-05-01 01:35:32 -0700

    added timing globals to the mix

---
+++ js/Globals/TimingGlobals.js
@@ -0,0 +1,10 @@
+/**
+ * Created by ajt on 5/1/2016.
+ */
+var TimingGlobals = (function TimingGlobals() {
+    var constants = {};
+
+    constants.startButtonPressed = false;
+
+    return constants;
+})();
