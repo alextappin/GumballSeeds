cf078335c0bea53f91cafb8e5e84d7c9f393486b
alextappin <alextappin@gmail.com>
2016-05-01 11:53:14 -0700

    reverted

--- js/UnitsSprites/LoadImage.js
+++ js/UnitsSprites/LoadImage.js
@@ -36,7 +36,7 @@ LoadImage.prototype.updateSprites = function(imageObj) {
 
     imageObj.alpha += this.Properties.alphaIncrement;
 
-    if (MapGlobals.soundLoaded && imageObj.alpha < this.Properties.alphaStart) { //once load screen is done doing its thing and sounds loaded.
+    if (MapGlobals.soundLoaded && imageObj.alpha < this.Properties.alphaStart) {
         HelperFunctions().switchToTitle();
         HelperFunctions().switchScreenToggle();
     }
