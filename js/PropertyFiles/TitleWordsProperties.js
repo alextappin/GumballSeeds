1fedde6307c70825398306ce46fde5455f405b87
alextappin <alextappin@gmail.com>
2016-05-01 03:14:27 -0700

    Titlewords properties filled out

--- js/PropertyFiles/TitleWordsProperties.js
+++ js/PropertyFiles/TitleWordsProperties.js
@@ -1,3 +1,13 @@
 /**
  * Created by ajt on 5/1/2016.
  */
+function TitleWordsProperties() {
+    var props = {
+        spriteCount : 0,
+        textures : []
+    };
+
+    return props;
+}
+
+TitleWordsProperties.constructor = TitleWordsProperties;
