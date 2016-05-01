e78d5b9a5c0b19f767705fe74ef2d88fd52ad08c
alextappin <alextappin@gmail.com>
2016-05-01 02:12:37 -0700

    more code cleanup

--- js/Scenes/TitleScreen.js
+++ js/Scenes/TitleScreen.js
@@ -17,9 +17,6 @@ TitleScreen.prototype.initializePositions = function() {
 };
 
 TitleScreen.prototype.constructTitleScreen = function(stage) {
-    stage.addChild(this.Properties.far); //Add to stage the units
-    stage.addChild(this.Properties.mid);
-    stage.addChild(this.Properties.mid2);
     stage.addChild(this.Properties.titleBoard);
     stage.addChild(this.Properties.startButton);
 };
@@ -27,5 +24,4 @@ TitleScreen.prototype.constructTitleScreen = function(stage) {
 TitleScreen.prototype.update = function() {
     this.Properties.titleBoard.update(this.Properties.titleBoard);
     this.Properties.startButton.update(this.Properties.startButton);
-    this.Properties.textScore.update(this.Properties.textScore);
 };
