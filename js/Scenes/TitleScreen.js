6bb7a2fc4dd160f064cfb01e9bebbb74db5fbdbe
alextappin <alextappin@gmail.com>
2016-05-01 03:33:58 -0700

    title screen properties added the titleWords

--- js/Scenes/TitleScreen.js
+++ js/Scenes/TitleScreen.js
@@ -14,16 +14,18 @@ function TitleScreen(stage) {
 
 TitleScreen.prototype.initializePositions = function() {
     this.Properties.titleBoard.setPositionAndScale(this.Properties.titleBoard);
+    this.Properties.titleWords.setPositionAndScale(this.Properties.titleWords);
     this.Properties.startButton.setPositionAndScale(this.Properties.startButton);
 };
 
 TitleScreen.prototype.constructTitleScreen = function(stage) {
     stage.addChild(this.Properties.titleBoard);
+    stage.addChild(this.Properties.titleWords);
     stage.addChild(this.Properties.startButton);
 };
 
 TitleScreen.prototype.update = function() {
-    TitleTimingHelper().updateTitleObjects(this.Properties.titleBoard, this.Properties.startButton);
+    TitleTimingHelper().updateTitleObjects(this.Properties.titleBoard, this.Properties.titleWords, this.Properties.startButton);
     /*this.Properties.titleBoard.update(this.Properties.titleBoard);
     this.Properties.startButton.update(this.Properties.startButton);*/
 };
