08cdff2f862ba886a9e2c6128224f4d0fc24011e
alextappin <alextappin@gmail.com>
2016-05-02 15:16:15 -0700

    filled out a template.

--- js/PropertyFiles/StartAnimationProperties.js
+++ js/PropertyFiles/StartAnimationProperties.js
@@ -1,3 +1,16 @@
 /**
  * Created by ajt on 5/2/2016.
  */
+function StartAnimationProperties() {
+    var props = {
+        textures : [],
+        spriteCount : 0,
+        changeSpriteCounter : 0,
+        spriteSpeed : 40,
+        numberOfTextures : 1
+    };
+
+    return props;
+}
+
+StartAnimationProperties.constructor = StartAnimationProperties;
--- js/UnitsSprites/StartScreenSprites/StartAnimation.js
+++ js/UnitsSprites/StartScreenSprites/StartAnimation.js
@@ -1,3 +1,42 @@
 /**
  * Created by ajt on 5/2/2016.
  */
+function StartAnimation() {
+    PIXI.Container.call(this);
+    this.constructStartAnimation();
+}
+
+StartAnimation.constructor = StartAnimation;
+StartAnimation.prototype = Object.create(PIXI.Container.prototype);
+
+StartAnimation.prototype.constructStartAnimation = function() {
+    this.Properties = new StartAnimationProperties();
+    this.initiateStartAnimationSprites();
+};
+
+StartAnimation.prototype.setPositionAndScale = function(obj) {
+    ScalingGlobals.titleScreenScaleX = HelperFunctions().getCorrectScaleWidth(obj.width);
+    ScalingGlobals.titleScreenScaleY = HelperFunctions().getCorrectScaleHeight(obj.height);
+    obj.scale = HelperFunctions().getNewPoint(ScalingGlobals.titleScreenScaleX,ScalingGlobals.titleScreenScaleY);
+    obj.position =  HelperFunctions().getNewPoint(HelperFunctions().getScreenPositionMiddleWidth(obj.width), HelperFunctions().getScreenPositionMiddleHeight(obj.height));
+    obj.alpha = TimingGlobals.titleAlphaStart;
+};
+
+StartAnimation.prototype.initiateStartAnimationSprites = function() {
+    this.Properties.textures.push(
+        PIXI.Texture.fromFrame("titleBG")
+    );
+    this.addChild(new PIXI.Sprite(this.Properties.textures[this.Properties.spriteCount]));
+};
+
+StartAnimation.prototype.setSpriteToCurrentTexture = function(titleBoardObj) {
+    titleBoardObj.children[0].texture = this.Properties.textures[this.Properties.spriteCount];
+};
+
+StartAnimation.prototype.update = function(titleBoardObj) {
+    if (titleBoardObj.alpha + TimingGlobals.titleBgAlphaIncrement > 1) {
+        titleBoardObj.alpha = 1;
+    } else {
+        titleBoardObj.alpha += TimingGlobals.titleBgAlphaIncrement;
+    }
+};
