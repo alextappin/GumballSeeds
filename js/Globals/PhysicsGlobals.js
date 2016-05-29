d40d4096fffa8981d17eb22948b4e50c6a046c36
alextappin <alextappin@gmail.com>
2016-05-29 04:08:45 -0700

    making everything more constants and scalable

--- js/Globals/PhysicsGlobals.js
+++ js/Globals/PhysicsGlobals.js
@@ -5,18 +5,18 @@ var PhysicsGlobals = (function PhysicsGlobals() {
     var constants = {};
 
     constants.attackingTime = 0;
-    constants.characterGravity = .51;
-    constants.characterJumpVelocity = -16;
-    constants.characterJumpHighVelocity = -20;
-    constants.characterJumpAttackVelocity = -14;
-    constants.characterVelocityY = 5;
+    constants.characterGravity = MapGlobals.screenWidth/2509; // .51;
+    constants.characterJumpVelocity = MapGlobals.screenWidth/-80;//-16;
+    constants.characterJumpHighVelocity = MapGlobals.screenWidth/-64; //-20;
+    constants.characterJumpAttackVelocity = MapGlobals.screenWidth/-91.4; //-14;
+    constants.characterVelocityY = MapGlobals.screenWidth/256; //5;
     constants.applyFallingGravity = false;
     constants.characterAirborn = true;
     constants.characterHighJumping = false;
 
-    constants.characterRiseSpeed = MapGlobals.screenHeight * .012;
+    constants.characterRiseSpeed = MapGlobals.screenWidth * .012;
     constants.characterBoltSpeed = MapGlobals.screenWidth * .04;
-    constants.characterEndSuperVelocity = -25;
+    constants.characterEndSuperVelocity = MapGlobals.screenWidth/-51.2; //-25;
 
     return constants;
 })();
