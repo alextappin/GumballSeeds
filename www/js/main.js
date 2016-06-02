5eee3a629f513cb22ff1cf33f0f536d5d1259fcf
alextappin <alextappin@gmail.com>
2016-06-02 15:51:07 -0700

    removed all the huge files and images. No need

--- www/js/main.js
+++ www/js/main.js
@@ -16,32 +16,7 @@ Main.prototype.refresh = function() {
 };
 
 Main.prototype.loadSpriteSheet = function() {
-    var assetsToLoad = ["resources/characterSprites.json", "resources/enemy.json", "resources/fgNew.json",
-        "resources/gumballStem.json", "resources/powerBar.json",
-        "resources/StartButton.json", "resources/trans.json",
-
-        "updatedImages/titleScreen/startButtons.json", "updatedImages/titleScreen/startrainbowanimations.json","updatedImages/titleScreen/startrainbowanimationwhite.json",
-        "updatedImages/titleScreen/titleWords.json", "updatedImages/titleScreen/titleBG.json", "updatedImages/map/gumballs.json",
-        "updatedImages/map/skyhaze1.json", "updatedImages/map/clouds.json", "updatedImages/map/gumballMachine.json",
-        "updatedImages/map/hill1.json", "updatedImages/map/hill2.json", "updatedImages/map/hazes.json",
-        "updatedImages/map/hill3.json", "updatedImages/map/ground17.json", "updatedImages/character/characterRun.json",
-        "updatedImages/character/characterJump.json", "updatedImages/character/characterAttack.json",
-        "updatedImages/enemy/enemyFly.json", "updatedImages/gumballs/gumballs.json",
-        "updatedImages/super/rainbow1.json","updatedImages/super/rainbow2.json","updatedImages/super/rainbow3.json",
-        "updatedImages/super/rainbow4.json","updatedImages/super/rainbow5.json","updatedImages/super/rainbow6.json",
-        "updatedImages/super/rainbow7.json","updatedImages/super/rainbow8.json","updatedImages/super/rainbow9.json",
-        "updatedImages/super/rainbow10.json","updatedImages/super/rainbow11.json","updatedImages/super/rainbow12.json",
-        "updatedImages/super/rainbow13.json","updatedImages/super/rainbow14.json","updatedImages/super/rainbow15.json",
-        "updatedImages/super/superGumball1.json", "updatedImages/character/characterSuper1.json", "updatedImages/character/characterSuper2.json",
-        "updatedImages/super/super1.json", "updatedImages/super/super2.json", "updatedImages/super/super3.json",
-        "updatedImages/super/super4.json", "updatedImages/super/super5.json",
-        "updatedImages/interface/superBar1.json", "updatedImages/interface/superBar2.json", "updatedImages/interface/interface.json",
-        "updatedImages/titleScreen/loadscreen.json", "updatedImages/enemy/enemyDeath.json"
-    ],
-        assetsToLoad2 = [
-            "imagesSmall/titleInter.json", "imagesSmall/backCharInter.json"
-        ],
-        assetsToLoad3 = [
+    var assetsToLoad3 = [
             "imagesSmall/character.json", "imagesSmall/map.json",
             "imagesSmall/title.json", "imagesSmall/interface.json"
         ],
