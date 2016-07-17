b8e1342af2d505a498f57e948e18d0efdc2e4562
alextappin <alextappin@gmail.com>
2016-07-17 16:32:15 -0700

    cleanup comments

--- www/js/ParallaxControl/Haze11.js
+++ www/js/ParallaxControl/Haze11.js
@@ -1,26 +1,6 @@
 /**
  * Created by ajt on 5/3/2016.
  */
-/*function Haze11() {
-    var texture = PIXI.Texture.fromFrame("11 haze2");
-    PIXI.extras.TilingSprite.call(this, texture, texture.width, texture.height);
-
-    var scale = MainGlobals.Helpers.getCorrectScaleWidth(this.width);
-    this.scale = MainGlobals.Helpers.getNewPoint(scale ,scale);
-    this.position = MainGlobals.Helpers.getNewPoint(0,MainGlobals.Scroller.haze11Y);
-    this.tilePosition = MainGlobals.Helpers.getNewPoint(0,0);
-    this.viewportX = 0;
-}
-
-Haze11.constructor = Haze11;
-Haze11.prototype = Object.create(PIXI.extras.TilingSprite.prototype);
-
-Haze11.prototype.update = function(newViewportX) {
-    this.tilePosition.x -= ((newViewportX - this.viewportX) * MainGlobals.Scroller.deltaX11); //distance traveled * change
-    this.viewportX = newViewportX;
-};*/
-
-
 function Haze11() {
     PIXI.Container.call(this);
     var texture = PIXI.Texture.fromFrame("11 haze2");
--- www/js/ParallaxControl/Haze14.js
+++ www/js/ParallaxControl/Haze14.js
@@ -1,26 +1,6 @@
 /**
  * Created by ajt on 5/3/2016.
  */
-/*function Haze14() {
-    var texture = PIXI.Texture.fromFrame("14 haze3");
-    PIXI.extras.TilingSprite.call(this, texture, texture.width, texture.height);
-
-    var scale = MainGlobals.Helpers.getCorrectScaleWidth(this.width);
-    this.scale = MainGlobals.Helpers.getNewPoint(scale ,scale);
-    this.position = MainGlobals.Helpers.getNewPoint(0,MainGlobals.Scroller.haze14Y);
-    this.tilePosition = MainGlobals.Helpers.getNewPoint(0,0);
-    this.viewportX = 0;
-}
-
-Haze14.constructor = Haze14;
-Haze14.prototype = Object.create(PIXI.extras.TilingSprite.prototype);
-
-Haze14.prototype.update = function(newViewportX) {
-    this.tilePosition.x -= ((newViewportX - this.viewportX) * MainGlobals.Scroller.deltaX14Haze); //distance traveled * change
-    this.viewportX = newViewportX;
-};*/
-
-
 function Haze14() {
     PIXI.Container.call(this);
     var texture = PIXI.Texture.fromFrame("14 haze3");
